aefc9fd39140a7010806839e556bd60d4b3ca65e
Tim <tim@skypanther.com>
2018-08-10 16:02:43 -0400

    update/simplify auto-calibrate script

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # robovision
 Vision processing scripts
+
+
+## auto_calibrate.py &mdash; Camera calibration utility
+
+1. Takes a still image every X seconds. Prompts you with a countdown between each.
+2. From the resulting images, selects N images, checking each in turn that it shows
+   the checkboard.
+3. From that set of stills, it calculates camera parameters and saves them to a
+   pickled object for later use in undistortion operations.
+4. Optionally, shows a preview of unwarping a random calibration photo using the
+   calculated lens parameters
+
+Usage: `python3 auto_calibrate.py [options]`
+
+Optional parameters
+
+* `-i num` = interval between captures, default `3`
+* `-t num` = total number of photos to capture, default `40`
+* `-n num` = min number of checkerboard photos to process, default `15`
+* `-o string` = output dir for calibration images and pickled props, default `./auto_calibrate/`
+* `-c (num|string)` = camera source, default is built-in webcam
+* `-p (true|false)` = Dewarp a test image and display a preview, default `True`
+* `-z (true|false)` = Skip image capture, just process previously captured calibration images, default `False`
+
+Examples:
+
+`python3 auto_calibrate.py -i 2 -n 20`
+
+(capture 20 stills, one every 2 seconds)
+
+`python3 auto_calibrate.py -o custom_folder`
+
+(saved captured images and results to ./custom_folder)
+
+`python3 auto_calibrate.py -o custom_folder -z true`
+
+(process photos already captured and stored in custom_folder)
+
+![Example results](auto_calibrate.png)
+
+(Note to self: don't look so bored next time! :smile:)
--- auto_calibrate.py
+++ auto_calibrate.py
@@ -1,18 +1,22 @@
 """
 Auto-calibrate: Camera calibration utility
 
-Take a still image every X seconds
-Divide the resulting images into Y “buckets"
-From each of the buckets, select Z images
-Use that set of stills to calculate camera parameters
-Save the intrinsic camera properties (pickled object?) for later use in undistortion operations
+1. Takes a still image every X seconds. Prompts you with a countdown between each.
+2. From the resulting images, selects N images, checking each in turn that it shows
+   the checkboard.
+3. From that set of stills, it calculates camera parameters and saves them to a
+   pickled object for later use in undistortion operations.
+4. Optionally, shows a preview of unwarping a random calibration photo using the
+   calculated lens parameters
 
 Params:
-X = interval, default 2
-Y = subsets, default 3
-Z = images per subset, default 5
-O = output dir for calibration images and pickled props, default './auto_calibrate/*'
-
+i = interval between captures, default 3
+t = total number of photos to capture, default 40
+n = min number of checkerboard photos to process, default 15
+o = output dir for calibration images and pickled props, default './auto_calibrate/*'
+c = camera source, default is built-in webcam
+p = Dewarp a test image and display a preview, default True
+z = Skip image capture, just process previously captured calibration images, default False
 
 """
 
@@ -20,17 +24,20 @@ import argparse
 import numpy as np
 import cv2
 import glob
+import imutils
 import os
 import pickle
+import random
 from time import sleep
 
 cam = None
-subset_messages = [
-    'the LEFT third of the field of view',
-    'the CENTER third of the field of view',
-    'the RIGHT third of the field of view',
-    'ANOTHER section of the field of view'
-]
+messages = {
+    'intro': 'Beginning image capture',
+    'move': 'Move the checkerboard elsewhere in the field of view',
+    'no_calibration': 'No valid calibration images found',
+    'capture_done': 'Done capturing. Beginning calibration...'
+}
+
 # Count of INNER CORNERS of target grid
 checkerboard_rows = 6
 checkerboard_colummns = 9
@@ -41,28 +48,35 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('-i', '--interval', default=3,
                     help='Interval in seconds between image captures')
-    ap.add_argument('-s', '--subsets', default=3,
-                    help='Subsets (divisions) of the captured images')
+    ap.add_argument('-t', '--total', default=40,
+                    help='Total number of photos to capture')
     ap.add_argument('-n', '--num', default=5,
-                    help='Number of images per subset to use in calibration')
+                    help='Number of images to use in calibration')
     ap.add_argument('-o', '--output', default='auto_calibrate',
-                    help='Path to output folder')
+                    help='Path to where captured images and results should be saved')
     ap.add_argument('-c', '--camera', default='default',
                     help='Camera source, default is built-in webcam')
-    ap.add_argument('-p', '--preview', default=True,
+    ap.add_argument('-p', '--preview', default=True, type=str2bool,
                     help='Dewarp test image and display a preview')
+    ap.add_argument('-z', '--skip', default=False, type=str2bool,
+                    help='Skip image capture, just calibrate from existing set')
     args = vars(ap.parse_args())
-    output_dir = get_output_dir(args['output'])
+    num_images = int(args['num'])
+    total = int(args['total'])
+    output_dir = get_output_dir(args['output'], args['skip'])
     source = 0 if args['camera'] is 'default' else args['camera']
-    cam = cv2.VideoCapture(source)
-    countdown()
-    for i in range(args['subsets']):
-        show_subset_message(i)
-        for n in range(args['num']):
-            take_photo(source, output_dir, i, n)
+    if args['skip'] is False:
+        cam = cv2.VideoCapture(source)
+        countdown()
+        show_message('intro')
+        sleep(3)
+        for i in range(total):
+            take_photo(source, output_dir, i, total)
+            show_message('move')
             sleep(args['interval'])
-    print('Done capturing. Beginning calibration...')
-    calibrate(output_dir, args['preview'])
+            clear_screen()
+        show_message('capture_done')
+    calibrate(output_dir, num_images, args['preview'])
 
 
 class Object(object):
@@ -72,35 +86,40 @@ class Object(object):
     pass
 
 
-def show_subset_message(subset_index):
-    if subset_index >= len(subset_messages):
-        subset_index = 3
-    print(f'MOVE the target to {subset_messages[subset_index]}')
+def show_message(key):
+    # shows a message from the messages dict
+    if key in messages:
+        print(messages[key])
     sleep(1)
 
 
 def countdown():
+    clear_screen()
     print('Taking a picture in:')
-    print('3')
+    print('3... ')
     sleep(1)
-    print('2')
+    print('2... ')
     sleep(1)
     print('1')
     sleep(1)
+    clear_screen()
 
 
-def take_photo(source, dirname, subset, image_num):
+def clear_screen():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+
+def take_photo(source, dirname, image_num, total):
     # TODO: handle source other than 0/webcam
     global cam
     image_captured, image = cam.read()
     if (image_captured):
-        fname = os.path.join(dirname, f'image_{subset}_{image_num}') + '.jpg'
+        fname = os.path.join(dirname, f'image_{image_num}') + '.jpg'
         cv2.imwrite(fname, image)
-        print(f'Photo saved to {dirname}/image_{subset}_{image_num}')
-        print('Move the target to a new location & orientation')
+        print(f'Captured image {image_num} of {total}')
 
 
-def get_output_dir(dirname):
+def get_output_dir(dirname, skip_image_capture):
     full_path = ''
     if dirname[0:1] == '/':
         # looks like an absolute path already
@@ -112,6 +131,9 @@ def get_output_dir(dirname):
         # just a bare name
         full_path = os.path.join(os.path.realpath('.'), dirname)
 
+    if skip_image_capture:
+        return full_path
+
     if os.path.isdir(full_path) is True:
         print('Output directory already exists. Please use a different directory.')
         exit(1)
@@ -119,7 +141,45 @@ def get_output_dir(dirname):
     return full_path
 
 
-def calibrate(dirname, preview):
+def get_calibration_imageset(dirname, num_images):
+    """
+    Return num_images calibration images from dirname that show the checkerboard
+
+    :@param dirname: Path to images
+    :@param num_images: Num images to select
+    :return: List of tuples of (image, corners)
+    """
+    images = glob.glob(f'{dirname}/*.jpg')
+    selected_images = []
+    iteration = 0
+    max_iterations = num_images * 4
+    while len(selected_images) < num_images:
+        if iteration >= max_iterations:
+            break
+        iteration += 1
+        candidate = random.choice(images)
+        img = cv2.imread(candidate)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (checkerboard_colummns, checkerboard_rows), None)
+        if ret is True:
+            selected_images.append((candidate, corners))
+    return selected_images
+
+
+def str2bool(v):
+    """
+    From https://stackoverflow.com/a/43357954/292947
+    Converts the argparse true/false to actual booleans
+    """
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def calibrate(dirname, min_images, preview):
     """
     Determine camera calibration parameters
     Based on https://docs.opencv.org/3.1.0/dc/dbb/tutorial_py_calibration.html
@@ -137,22 +197,18 @@ def calibrate(dirname, preview):
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
 
-    images = glob.glob(f'{dirname}/*.jpg')
+    image_set = get_calibration_imageset(dirname, min_images)
+    if len(image_set) == 0:
+        show_message('no_calibration')
+        exit()
 
-    for absolute_filename in images:
-        img = cv2.imread(absolute_filename)
-        fname = absolute_filename.split(os.path.sep)[-1]
+    for calibration_set in image_set:
+        # calibration_set is a tuple of (image, corners)
+        img = cv2.imread(calibration_set[0])
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (checkerboard_colummns, checkerboard_rows), None)
-        print(f'Found checkboard in {fname} -> {ret}')
-
-        # If found, add object points, image points (after refining them)
-        if ret is True:
-            objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray, calibration_set[1], (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
 
     if not objpoints:
         print('Target not detected in any of the photos')
@@ -161,14 +217,17 @@ def calibrate(dirname, preview):
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     # print(ret, mtx, dist, rvecs, tvecs)
 
-    img = cv2.imread(os.path.join(dirname, 'image_0_0.jpg'))
-    cv2.imshow('Original', img)
     h, w = img.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 
-    # undistort
-    dst_one = cv2.undistort(img, mtx, dist, None, newcameramtx)
-    cv2.imshow('Remapped Image', dst_one)
+    if preview:
+        # Show original and corrected image
+        candidate = random.choice(image_set)
+        img = cv2.imread(candidate[0])
+        cv2.imshow('Original', imutils.resize(img, height=320))
+        corrected = cv2.undistort(img, mtx, dist, None, newcameramtx)
+        cv2.imshow('Remapped Image', imutils.resize(corrected, height=320))
+        cv2.waitKey(0)
 
     total_error = 0
     for i in range(len(objpoints)):
@@ -188,7 +247,7 @@ def calibrate(dirname, preview):
     camera_params.mean_accuracy = mean_accuracy
 
     param_file_name = os.path.join(dirname, 'params.pickle')
-    with open(param_file_name,'wb') as param_file:
+    with open(param_file_name, 'wb') as param_file:
         pickle.dump(camera_params, param_file)
         print(f'Camera params saved to {param_file_name}')
 
