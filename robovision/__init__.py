6684a0d93697cb9b9217611d4076332ef8f0e794
Tim Poulsen <tim@skypanther.com>
2019-02-01 22:05:22 -0500

    rename utils to core, add Deck

--- robovision/__init__.py
+++ robovision/__init__.py
@@ -1,16 +1,17 @@
-from .utils import adjust_brightness
-from .utils import adjust_brightness_contrast
-from .utils import adjust_contrast
-from .utils import detect_edges
-from .utils import equalize
-from .utils import flatten
-from .utils import get_video_stream
-from .utils import load_camera_params
-from .utils import resize
-from .utils import resize_raw
+# Core functions accessed like robovision.resize()
+from .core import adjust_brightness           # noqa # pylint: disable=unused-import
+from .core import adjust_brightness_contrast  # noqa # pylint: disable=unused-import
+from .core import adjust_contrast             # noqa # pylint: disable=unused-import
+from .core import detect_edges                # noqa # pylint: disable=unused-import
+from .core import equalize                    # noqa # pylint: disable=unused-import
+from .core import flatten                     # noqa # pylint: disable=unused-import
+from .core import get_video_stream            # noqa # pylint: disable=unused-import
+from .core import load_camera_params          # noqa # pylint: disable=unused-import
+from .core import resize                      # noqa # pylint: disable=unused-import
+from .core import resize_raw                  # noqa # pylint: disable=unused-import
 
-from .video_stream import VideoStream
-
-from .preprocessor import Preprocessor
-
-from .target import Target
+# Sub-libraries accessed like robovision.video_stream.function_name()
+from .deck import Deck                        # noqa # pylint: disable=unused-import
+from .preprocessor import Preprocessor        # noqa # pylint: disable=unused-import
+from .target import Target                    # noqa # pylint: disable=unused-import
+from .video_stream import VideoStream         # noqa # pylint: disable=unused-import
